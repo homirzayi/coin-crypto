d65d40923d468632c30ddb9d904d4ad046dac234
homirzayi <homirzayi@hotmail.com>
2023-10-25 13:40:41 +0330

    modified App.jsx and import css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import "../src/styles/dist/styles.css";
+import "./styles/dist/styles.css";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
